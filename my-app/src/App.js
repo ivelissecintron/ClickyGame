1a31861b873324ce57b4f8d97a46eca35182b6b2
Ivelisse <msivelisse@gmail.com>
2018-07-31 16:41:20 -0500

    create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Clicky Game: The Office</h1>
+          <h2 className="Instructions">Click on an image to earn points, but don't click on any image more than once!</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
