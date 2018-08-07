b13346900c056e053226b49c256794c761c5d0c7
Ivelisse <msivelisse@gmail.com>
2018-08-07 11:57:52 -0500

    updated css

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -5,6 +5,9 @@ const Navbar = props => (
 
 <div >
     <nav className="navbar fixed-top">
+        <h1 className="App-title">The Office Clicky Game</h1>
+          <h2 className="Instructions">Click on an image to earn points, but don't click on any image more than once!</h2>
+
         <ul>
             <li className="title score">
                 Score: {props.score} | High Score: {props.highscore}
